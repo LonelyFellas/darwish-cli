05badb643a7840bfd76e90544e4302f3e8c712a0
worksapce <289582304@qq.com>
2024-03-17 21:36:08 +0800

    chore: delete unless code

--- packages/create-darwish-cli/src/index.ts
+++ packages/create-darwish-cli/src/index.ts
@@ -2,18 +2,18 @@ import fs from "node:fs";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
 import minimist from "minimist";
-import {
-  blue,
-  cyan,
-  green,
-  lightBlue,
-  lightGreen,
-  lightRed,
-  magenta,
-  red,
-  reset,
-  yellow,
-} from "kolorist";
+// import {
+//   blue,
+//   cyan,
+//   green,
+//   lightBlue,
+//   lightGreen,
+//   lightRed,
+//   magenta,
+//   red,
+//   reset,
+//   yellow,
+// } from "kolorist";
 
 // Avoids autoconversion to number of the project name by defining that the args
 // non associated with an option ( _ ) needs to be parsed as a string. See #4606
@@ -23,220 +23,220 @@ const argv = minimist<{
 }>(process.argv.slice(2), { string: ["_"] });
 const cwd = process.cwd();
 
-type ColorFunc = (str: string | number) => string;
-type Framework = {
-  name: string;
-  display: string;
-  color: ColorFunc;
-  variants: FrameworkVariant[];
-};
-type FrameworkVariant = {
-  name: string;
-  display: string;
-  color: ColorFunc;
-  customCommand?: string;
-};
-const FRAMEWORKS: Framework[] = [
-  {
-    name: "vanilla",
-    display: "Vanilla",
-    color: yellow,
-    variants: [
-      {
-        name: "vanilla-ts",
-        display: "TypeScript",
-        color: blue,
-      },
-      {
-        name: "vanilla",
-        display: "JavaScript",
-        color: yellow,
-      },
-    ],
-  },
-  {
-    name: "vue",
-    display: "Vue",
-    color: green,
-    variants: [
-      {
-        name: "vue-ts",
-        display: "TypeScript",
-        color: blue,
-      },
-      {
-        name: "vue",
-        display: "JavaScript",
-        color: yellow,
-      },
-      {
-        name: "custom-create-vue",
-        display: "Customize with create-vue ↗",
-        color: green,
-        customCommand: "npm create vue@latest TARGET_DIR",
-      },
-      {
-        name: "custom-nuxt",
-        display: "Nuxt ↗",
-        color: lightGreen,
-        customCommand: "npm exec nuxi init TARGET_DIR",
-      },
-    ],
-  },
-  {
-    name: "react",
-    display: "React",
-    color: cyan,
-    variants: [
-      {
-        name: "react-ts",
-        display: "TypeScript",
-        color: blue,
-      },
-      {
-        name: "react-swc-ts",
-        display: "TypeScript + SWC",
-        color: blue,
-      },
-      {
-        name: "react",
-        display: "JavaScript",
-        color: yellow,
-      },
-      {
-        name: "react-swc",
-        display: "JavaScript + SWC",
-        color: yellow,
-      },
-      {
-        name: "custom-remix",
-        display: "Remix ↗",
-        color: cyan,
-        customCommand:
-          "npm create remix@latest TARGET_DIR -- --template remix-run/remix/templates/vite",
-      },
-    ],
-  },
-  {
-    name: "preact",
-    display: "Preact",
-    color: magenta,
-    variants: [
-      {
-        name: "preact-ts",
-        display: "TypeScript",
-        color: blue,
-      },
-      {
-        name: "preact",
-        display: "JavaScript",
-        color: yellow,
-      },
-    ],
-  },
-  {
-    name: "lit",
-    display: "Lit",
-    color: lightRed,
-    variants: [
-      {
-        name: "lit-ts",
-        display: "TypeScript",
-        color: blue,
-      },
-      {
-        name: "lit",
-        display: "JavaScript",
-        color: yellow,
-      },
-    ],
-  },
-  {
-    name: "svelte",
-    display: "Svelte",
-    color: red,
-    variants: [
-      {
-        name: "svelte-ts",
-        display: "TypeScript",
-        color: blue,
-      },
-      {
-        name: "svelte",
-        display: "JavaScript",
-        color: yellow,
-      },
-      {
-        name: "custom-svelte-kit",
-        display: "SvelteKit ↗",
-        color: red,
-        customCommand: "npm create svelte@latest TARGET_DIR",
-      },
-    ],
-  },
-  {
-    name: "solid",
-    display: "Solid",
-    color: blue,
-    variants: [
-      {
-        name: "solid-ts",
-        display: "TypeScript",
-        color: blue,
-      },
-      {
-        name: "solid",
-        display: "JavaScript",
-        color: yellow,
-      },
-    ],
-  },
-  {
-    name: "qwik",
-    display: "Qwik",
-    color: lightBlue,
-    variants: [
-      {
-        name: "qwik-ts",
-        display: "TypeScript",
-        color: lightBlue,
-      },
-      {
-        name: "qwik",
-        display: "JavaScript",
-        color: yellow,
-      },
-      {
-        name: "custom-qwik-city",
-        display: "QwikCity ↗",
-        color: lightBlue,
-        customCommand: "npm create qwik@latest basic TARGET_DIR",
-      },
-    ],
-  },
-  {
-    name: "others",
-    display: "Others",
-    color: reset,
-    variants: [
-      {
-        name: "create-vite-extra",
-        display: "create-vite-extra ↗",
-        color: reset,
-        customCommand: "npm create vite-extra@latest TARGET_DIR",
-      },
-      {
-        name: "create-electron-vite",
-        display: "create-electron-vite ↗",
-        color: reset,
-        customCommand: "npm create electron-vite@latest TARGET_DIR",
-      },
-    ],
-  },
-];
-const TEMPLATES = FRAMEWORKS.map(
-  (f) => (f.variants && f.variants.map((v) => v.name)) || [f.name]
-).reduce((a, b) => a.concat(b), []);
+// type ColorFunc = (str: string | number) => string;
+// type Framework = {
+//   name: string;
+//   display: string;
+//   color: ColorFunc;
+//   variants: FrameworkVariant[];
+// };
+// type FrameworkVariant = {
+//   name: string;
+//   display: string;
+//   color: ColorFunc;
+//   customCommand?: string;
+// };
+// const FRAMEWORKS: Framework[] = [
+//   {
+//     name: "vanilla",
+//     display: "Vanilla",
+//     color: yellow,
+//     variants: [
+//       {
+//         name: "vanilla-ts",
+//         display: "TypeScript",
+//         color: blue,
+//       },
+//       {
+//         name: "vanilla",
+//         display: "JavaScript",
+//         color: yellow,
+//       },
+//     ],
+//   },
+//   {
+//     name: "vue",
+//     display: "Vue",
+//     color: green,
+//     variants: [
+//       {
+//         name: "vue-ts",
+//         display: "TypeScript",
+//         color: blue,
+//       },
+//       {
+//         name: "vue",
+//         display: "JavaScript",
+//         color: yellow,
+//       },
+//       {
+//         name: "custom-create-vue",
+//         display: "Customize with create-vue ↗",
+//         color: green,
+//         customCommand: "npm create vue@latest TARGET_DIR",
+//       },
+//       {
+//         name: "custom-nuxt",
+//         display: "Nuxt ↗",
+//         color: lightGreen,
+//         customCommand: "npm exec nuxi init TARGET_DIR",
+//       },
+//     ],
+//   },
+//   {
+//     name: "react",
+//     display: "React",
+//     color: cyan,
+//     variants: [
+//       {
+//         name: "react-ts",
+//         display: "TypeScript",
+//         color: blue,
+//       },
+//       {
+//         name: "react-swc-ts",
+//         display: "TypeScript + SWC",
+//         color: blue,
+//       },
+//       {
+//         name: "react",
+//         display: "JavaScript",
+//         color: yellow,
+//       },
+//       {
+//         name: "react-swc",
+//         display: "JavaScript + SWC",
+//         color: yellow,
+//       },
+//       {
+//         name: "custom-remix",
+//         display: "Remix ↗",
+//         color: cyan,
+//         customCommand:
+//           "npm create remix@latest TARGET_DIR -- --template remix-run/remix/templates/vite",
+//       },
+//     ],
+//   },
+//   {
+//     name: "preact",
+//     display: "Preact",
+//     color: magenta,
+//     variants: [
+//       {
+//         name: "preact-ts",
+//         display: "TypeScript",
+//         color: blue,
+//       },
+//       {
+//         name: "preact",
+//         display: "JavaScript",
+//         color: yellow,
+//       },
+//     ],
+//   },
+//   {
+//     name: "lit",
+//     display: "Lit",
+//     color: lightRed,
+//     variants: [
+//       {
+//         name: "lit-ts",
+//         display: "TypeScript",
+//         color: blue,
+//       },
+//       {
+//         name: "lit",
+//         display: "JavaScript",
+//         color: yellow,
+//       },
+//     ],
+//   },
+//   {
+//     name: "svelte",
+//     display: "Svelte",
+//     color: red,
+//     variants: [
+//       {
+//         name: "svelte-ts",
+//         display: "TypeScript",
+//         color: blue,
+//       },
+//       {
+//         name: "svelte",
+//         display: "JavaScript",
+//         color: yellow,
+//       },
+//       {
+//         name: "custom-svelte-kit",
+//         display: "SvelteKit ↗",
+//         color: red,
+//         customCommand: "npm create svelte@latest TARGET_DIR",
+//       },
+//     ],
+//   },
+//   {
+//     name: "solid",
+//     display: "Solid",
+//     color: blue,
+//     variants: [
+//       {
+//         name: "solid-ts",
+//         display: "TypeScript",
+//         color: blue,
+//       },
+//       {
+//         name: "solid",
+//         display: "JavaScript",
+//         color: yellow,
+//       },
+//     ],
+//   },
+//   {
+//     name: "qwik",
+//     display: "Qwik",
+//     color: lightBlue,
+//     variants: [
+//       {
+//         name: "qwik-ts",
+//         display: "TypeScript",
+//         color: lightBlue,
+//       },
+//       {
+//         name: "qwik",
+//         display: "JavaScript",
+//         color: yellow,
+//       },
+//       {
+//         name: "custom-qwik-city",
+//         display: "QwikCity ↗",
+//         color: lightBlue,
+//         customCommand: "npm create qwik@latest basic TARGET_DIR",
+//       },
+//     ],
+//   },
+//   {
+//     name: "others",
+//     display: "Others",
+//     color: reset,
+//     variants: [
+//       {
+//         name: "create-vite-extra",
+//         display: "create-vite-extra ↗",
+//         color: reset,
+//         customCommand: "npm create vite-extra@latest TARGET_DIR",
+//       },
+//       {
+//         name: "create-electron-vite",
+//         display: "create-electron-vite ↗",
+//         color: reset,
+//         customCommand: "npm create electron-vite@latest TARGET_DIR",
+//       },
+//     ],
+//   },
+// ];
+// const TEMPLATES = FRAMEWORKS.map(
+//   (f) => (f.variants && f.variants.map((v) => v.name)) || [f.name]
+// ).reduce((a, b) => a.concat(b), []);
 const renameFiles: Record<string, string | undefined> = {
   _gitignore: ".gitignore",
 };
@@ -245,7 +245,7 @@ const defaultTargetDir = "vite-project";
 async function init() {
   const argTargetDir = formatTargetDir(argv._[0]);
   let targetDir = argTargetDir || defaultTargetDir;
-  const argTemplate = argv.template || argv.t;
+  // const argTemplate = argv.template || argv.t;
 
   // determine template
   // let template: string = variant || framework?.name || argTemplate
