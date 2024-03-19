818629471f60f64ce8d37539a082089f82bba51d
worksapce <289582304@qq.com>
2024-03-19 11:48:57 +0800

    refactor: modity build.config files

--- packages/ddaarr/feature.md
+++
@@ -1,6 +0,0 @@
-# Feature
-
-* modify package.json's name
-* add terminal choice 
-* create multi-template 
-* add git clone template mod
--- packages/ddaarr/rollup.config.mjs
+++ packages/ddaarr/rollup.config.mjs
@@ -1,10 +1,13 @@
 import typescript from "@rollup/plugin-typescript";
+import terser from "@rollup/plugin-terser"
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import commonjs from '@rollup/plugin-commonjs'; 
 
 export default {
   input: "src/index.ts",
   output: {
     file: "bin/index.mjs",
-    format: "esm"
+    format: "esm",
   }, 
-  plugins: [typescript()]
+  plugins: [nodeResolve(), typescript(), terser(), commonjs()]
 }
